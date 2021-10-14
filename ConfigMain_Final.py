1dc33e8a9ab73269dfbc7c07c8fcca9f7e84a186
hellothisisandrew <60022764+hellothisisandrew@users.noreply.github.com>
2021-10-13 22:52:24 -0700

    Update ConfigMain_Final.py

--- ConfigMain_Final.py
+++ ConfigMain_Final.py
@@ -1,7 +1,7 @@
 import logic
 
 
-hello = logic.display_instance(input("What is the name of the file you want to check:: ").replace(".xlsm","") + ".xlsm")
+hello = logic.display_instance(input("What is the name of the file:: ").replace(".xlsm","") + ".xlsm")
 hello.configGen()
 
 
